c58db09b6211b2fed207249f7aa670616aaf0d67
lius <edmondLau1018@foxmail.com>
2024-04-04 10:06:58 +0800

    新增 mixin 测试混入

--- vue_project_demos/project_demo1/src/components/School.vue
+++ vue_project_demos/project_demo1/src/components/School.vue
@@ -2,12 +2,16 @@
   <div class="school">
     <h3>学校名称：{{name}}</h3>
     <h3>学校地址：{{address}}</h3>
+    <button @click="showName">火速展示</button>
   </div>
 </template>
 
 <script>
+import {DemoMixin} from "@/DemoMixin";
+
 export default {
   name: "School",
+  mixins: [DemoMixin],
   data(){
    return {
      name: '牛牛学院',
--- vue_project_demos/project_demo1/src/components/Student.vue
+++ vue_project_demos/project_demo1/src/components/Student.vue
@@ -3,12 +3,15 @@
     <h3>学生名称：{{ name }}</h3>
     <h3>学生性别：{{ sexJob }}</h3>
     <h3>展示 whoreName : {{whoreName}}</h3>
+    <button @click="showName">火速展示</button>
   </div>
 </template>
 
 <script>
+import {DemoMixin} from "../DemoMixin";
 export default {
   name: "Student",
+  mixins: [DemoMixin],
   props: {
     whoreName: {
       type: String,
