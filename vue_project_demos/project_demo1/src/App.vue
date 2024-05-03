2eec4a1b74bec82f943994957be7e790cc9fa612
lius <edmondLau1018@foxmail.com>
2024-05-03 09:47:27 +0800

    具名插槽

--- vue_project_demos/project_demo1/src/App.vue
+++ vue_project_demos/project_demo1/src/App.vue
@@ -1,28 +1,32 @@
 <template>
   <div class="container">
-    <category title="美食">
-      <img src="https://s3.ax1x.com/2021/01/16/srJlq0.jpg" alt="">
-    </category>
-    <category title="游戏">
-      <ul>
-        <li v-for="(game,index) in games" :key="index">{{ game }}</li>
+    <category-name>
+      <ul slot="center">
+        <li v-for="(food,index) in foods" :key="index">{{ food }}</li>
       </ul>
-    </category>
-    <category title="电影">
-      <!-- controls 标签控制视频播放-->
-      <video
-          controls
-          src="http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"></video>
-    </category>
+      <img slot="footer" src="https://s3.ax1x.com/2021/01/16/srJlq0.jpg" alt="">
+    </category-name>
+    <category-name>
+      <!--使用 template 包裹元素可以省去一层 div
+      而且使用 template 可以使用插槽新的写法 v-slot:插槽名称-->
+      <template v-slot:center>
+        <ul>
+          <li v-for="(film,index) in films" :key="index">{{ film }}</li>
+        </ul>
+        <video src="http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4" controls></video>
+      </template>
+      <h4 slot="footer" style="text-align: center">欢迎观影</h4>
+    </category-name>
   </div>
 </template>
 
 <script>
 import Category from "@/components/Category";
+import CategoryName from "@/components/CategoryName";
 
 export default {
   name: 'App',
-  components: {Category},
+  components: {Category, CategoryName},
   data() {
     return {
       foods: ['火锅', '烧烤', '小龙虾', '牛排'],
---
+++ vue_project_demos/project_demo1/src/components/CategoryName.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="categoryName">
+    <h3>{{ title }}分类</h3>
+    <hr>
+    <!--具名插槽 ： 一个组件内有多个插槽，通过 name 属性指定插槽的名称
+    同样的 在调用方通过 slot 属性 或者 v-slot（仅适用于 <template> 标签）
+    指定将特定的DOM 元素填充到指定名称的插槽中-->
+    <slot name="center"></slot>
+    <slot name="footer"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CategoryName",
+  props: ['title']
+}
+</script>
+
+<style scoped>
+.categoryName {
+  background-color: skyblue;
+  width: 200px;
+  height: 300px;
+}
+
+h3 {
+  text-align: center;
+  background-color: orange;
+}
+
+video {
+  width: 100%;
+}
+
+img {
+  width: 100%;
+}
+</style>
