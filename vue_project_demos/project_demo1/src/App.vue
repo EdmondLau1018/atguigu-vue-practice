f30a746fe9b73c5554266a0beccc3fafee1e9fd0
lius <edmondLau1018@foxmail.com>
2024-05-03 16:33:27 +0800

    vuex 使用 store 修改共享数据

--- vue_project_demos/project_demo1/src/App.vue
+++ vue_project_demos/project_demo1/src/App.vue
@@ -1,44 +1,47 @@
 <template>
-  <div class="container">
-    <category-area title="游戏">
-      <!-- 插槽的使用者通过 scope 属性接收插槽传递的数据-->
-      <template scope="data">
-        <ul>
-          <li v-for="(game,index) in data.games" :key="index">{{game}}</li>
-        </ul>
-        <h4>{{data.message}}</h4>
-      </template>
-    </category-area>
-    <category-area title="游戏">
-      <!-- 插槽的使用者通过 scope 属性接收插槽传递的数据-->
-      <template slot-scope="data">
-        {{data}}
-      </template>
-    </category-area>
+  <div>
+    <h1>当前求和为：{{$store.state.sum}}</h1>
+    <select v-model.number="n">
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+    </select>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+    <button @click="incrementOdd">当前求和为奇数再加</button>
+    <button @click="incrementWait">等一等再加</button>
   </div>
 </template>
 
 <script>
-import Category from "@/components/Category";
-import CategoryName from "@/components/CategoryName";
-import CategoryArea from "@/components/CategoryArea";
-
 export default {
-  name: 'App',
-  components: {CategoryArea, Category, CategoryName},
+  name:'Count',
   data() {
+    return {
+      n:1, //用户选择的数字
+      sum:0 //当前的和
+    }
   },
-  mounted() {
+  methods: {
+    increment(){
+      this.$store.dispatch('add',this.n)
+    },
+    decrement(){
+      //  在没有业务逻辑的情况下可以直接通过 commit 调用 mutations 里的函数
+      this.$store.commit('REDUCE',this.n)
+    },
+    incrementOdd(){
+      this.$store.dispatch('addOdd',this.n)
+    },
+    incrementWait(){
+      this.$store.dispatch('addWait',this.n)
+    },
   },
-  methods: {},
-  watch: {}
 }
 </script>
 
-<style>
-/*调整页面布局*/
-.container {
-  display: flex;
-  justify-content: space-around;
+<style lang="css">
+button{
+  margin-left: 5px;
 }
 </style>
--- vue_project_demos/project_demo1/src/store/index.js
+++ vue_project_demos/project_demo1/src/store/index.js
@@ -1,14 +1,48 @@
+//  引入 Vue
 import Vue from 'vue'
 
+//  引入插件 VueX
 import Vuex from 'vuex'
 
+//  使用插件
 Vue.use(Vuex)
 
-const actions = {}
+//  创建 actions 对象
+const actions = {
+    add(context, value) {
+        console.log('actions接收上下文参数 context：', context, '接收到传递的参数', value);
+        //  通过 commit 调用 mutations 中的方法
+        context.commit('ADD', value)
+    },
+    addOdd(context, value) {
+        //  判断奇偶的业务逻辑
+        if (context.state.sum % 2) {
+            context.commit('ADD', value)
+        }
+    },
+    addWait(context,value){
+        setTimeout(function () {
+            context.commit('ADD',value)
+        },1000)
+    }
+}
 
-const mutations = {}
+//  创建 mutations 对象
+const mutations = {
+    ADD(state, value) {
+        console.log('mutations收到的参数 state： ', state, '参数值为：', value);
+        state.sum += value
+    },
+    REDUCE(state, value) {
+        state.sum -= value
+    },
 
-const state = {}
+}
+
+//  state 对象用于保存数据
+const state = {
+    sum: 0
+}
 
 export default new Vuex.Store({
     actions,
