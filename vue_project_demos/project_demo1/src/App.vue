4572c6c7266bc2d7354a84f9c9ffccd20bb97dd3
lius <edmondLau1018@foxmail.com>
2024-05-10 19:18:07 +0800

    vue-router 补充

--- vue_project_demos/project_demo1/src/App.vue
+++ vue_project_demos/project_demo1/src/App.vue
@@ -1,9 +1,7 @@
 <template>
   <div>
     <div class="row">
-      <div class="col-xs-offset-2 col-xs-8">
-        <div class="page-header"><h2>Vue Router Demo</h2></div>
-      </div>
+     <Banner/>
     </div>
     <div class="row">
       <div class="col-xs-2 col-xs-offset-2">
@@ -24,9 +22,10 @@
 </template>
 
 <script>
+import Banner from "./components/Banner";
 export default {
   name:'App',
-  components: {},
+  components: {Banner},
   data() {
   },
   computed:{
@@ -41,3 +40,11 @@ button{
   margin-left: 5px;
 }
 </style>
+
+<!--
+路由组件通常放在 pages 目录下
+一般组件通常放在 components 目录下
+通过切换 被切换走的组件会被销毁 ，有需要的组件才会被挂载
+每个路由组件都有自己的 route 信息  可以通过 $route 看到
+整个应用只有一个 router 可以通过 $router 看到
+-->
--- vue_project_demos/project_demo1/src/router/index.js
+++ vue_project_demos/project_demo1/src/router/index.js
@@ -1,8 +1,8 @@
 import VueRouter from "vue-router";
 
 //  引入相关组件
-import About from "../components/About";
-import Home from "../components/Home";
+import About from "../pages/About";
+import Home from "../pages/Home";
 
 export default new VueRouter({
     routes: [
